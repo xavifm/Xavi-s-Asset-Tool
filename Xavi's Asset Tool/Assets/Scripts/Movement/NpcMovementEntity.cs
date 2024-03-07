1dca8763269b672108514d8aff7104c3009e7889
xavifm <60983549+xavifm@users.noreply.github.com>
2024-03-07 19:24:24 +0100

    Uploaded the base entities system focused in player system

---
+++ Xavi's Asset Tool/Assets/Scripts/Movement/NpcMovementEntity.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMovementEntityFramework : MonoBehaviour
+{
+    public Movement MovementLogic { get; set; }
+}
