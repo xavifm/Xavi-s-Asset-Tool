c95fb4d4162b221814a8050886b22fdfb35487aa
xavifm <60983549+xavifm@users.noreply.github.com>
2024-03-12 12:51:44 +0100

    implemented the player movement logic

--- Xavi's Asset Tool/Assets/Scripts/Entities/Creatures/Player.cs
+++ Xavi's Asset Tool/Assets/Scripts/Entities/Creatures/Player.cs
@@ -10,5 +10,6 @@ public class Player : CreatureEntity
     {
         base.VirtualUpdate();
         MovementLogic.MovementLogic();
+        MovementLogic.RotationLogic();
     }
 }
--- Xavi's Asset Tool/Assets/Scripts/Movement/Base/Movement.cs
+++ Xavi's Asset Tool/Assets/Scripts/Movement/Base/Movement.cs
@@ -7,12 +7,20 @@ public class Movement : MonoBehaviour
     public Rigidbody EntityRb;
     public Animator CreatureAnimator;
 
+    [SerializeField] protected float Velocity;
+    [SerializeField] protected float ResetSpeed;
+
     public virtual void MovementStateMachine()
     {
 
     }
 
     public virtual void MovementLogic()
+    {
+        Vector3 currentVelocity = Vector3.zero;
+    }
+
+    public virtual void RotationLogic()
     {
 
     }
