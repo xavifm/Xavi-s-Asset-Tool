0978d38083ae8487849701f6585c59bc3ec63bdf
xavifm <60983549+xavifm@users.noreply.github.com>
2024-06-10 15:19:28 +0200

    built the new animation system

--- Xavi's Asset Tool/Assets/Scripts/Animation/EntityAnimator.cs
+++ Xavi's Asset Tool/Assets/Scripts/Animation/EntityAnimator.cs
@@ -9,17 +9,36 @@ public class EntityAnimator : MonoBehaviour
 
     public void SwitchAnimationState(string _state)
     {
+        DisableAllAnimations();
 
+        AnimationList animation = GetAnimation(_state);
+
+        if (animation != null)
+            animation.EnableAnimation(AnimatorEntity);
     }
 
     public void DisableAllAnimations()
     {
-
+        foreach(AnimationList animation in Animations)
+        {
+            animation.DisableAnimation(AnimatorEntity);
+        }
     }
 
     AnimationList GetAnimation(string _state)
     {
-        return new AnimationList();
+        AnimationList animationQuery = null;
+
+        foreach (AnimationList animation in Animations)
+        {
+            if (animation.key.Equals(_state))
+            {
+                animationQuery = animation;
+                break;
+            }
+        }
+
+        return animationQuery;
     }
 }
 
@@ -28,4 +47,16 @@ public class AnimationList
 {
     public string key; 
     public bool enabled;
+
+    public void EnableAnimation(Animator _animator) 
+    {
+        enabled = true;
+        _animator.SetBool(key, enabled);
+    }
+
+    public void DisableAnimation(Animator _animator)
+    {
+        enabled = false;
+        _animator.SetBool(key, enabled);
+    }
 }
