889546f07ee909978510dc908f359ba9a2386329
xavifm <60983549+xavifm@users.noreply.github.com>
2024-06-27 11:58:23 +0200

    added the new architecture

--- Xavi's Asset Tool/Assets/Scripts/Movement/NpcMovementEntity.cs
+++ Xavi's Asset Tool/Assets/Scripts/Movement/NpcMovementEntity.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class NpcMovementEntity : CreatureMovement
 {
+    public override void MovementLogic()
+    {
+        base.MovementLogic();
+
+
+    }
+
+    public override void RotationLogic()
+    {
+        base.RotationLogic();
+
+
+    }
+
     public override void AnimationLogic()
     {
         AnimatorEntity.SwitchAnimationState("Walk");
