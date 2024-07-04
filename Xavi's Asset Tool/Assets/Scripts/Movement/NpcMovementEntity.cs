6cd81e92895619fe7f5a1f9ad6d8c6c8015efebe
xavifm <60983549+xavifm@users.noreply.github.com>
2024-07-04 11:05:09 +0200

    modified the npc base movement logic expandable architecture

--- Xavi's Asset Tool/Assets/Scripts/Movement/NpcMovementEntity.cs
+++ Xavi's Asset Tool/Assets/Scripts/Movement/NpcMovementEntity.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class NpcMovementEntity : CreatureMovement
 {
-    public override void MovementLogic()
-    {
-        base.MovementLogic();
-
-
-    }
-
-    public override void RotationLogic()
-    {
-        base.RotationLogic();
-
-
-    }
+    protected enum AnimationStates { WALK, DAMAGE, DEAD, ATTACK }
+    protected Dictionary<AnimationStates, string> Animations;
+    protected AnimationStates CurrentAnimation;
 
     public override void AnimationLogic()
     {
-        AnimatorEntity.SwitchAnimationState("Walk");
+        if (Animations == null)
+            InitializeList();
+
+        if (Animations.TryGetValue(CurrentAnimation, out string animationQuery))
+            AnimatorEntity.SwitchAnimationState(animationQuery);
+
         AnimatorEntity.SwitchCharacterSpeedBlend(EntityRb.velocity.magnitude);
+    }
+
+    public virtual void InitializeList()
+    {
+        Animations.Add(AnimationStates.WALK, "Walk");
+        Animations.Add(AnimationStates.DAMAGE, "Damaged");
+        Animations.Add(AnimationStates.DEAD, "Dead");
+        Animations.Add(AnimationStates.ATTACK, "Attack");
     }
 }
